453bab51e56e03408a1332ede1acad7da4177809
zlxlwnfl@naver.com <zlxlwnfl@naver.com>
2023-05-29 02:23:33 +0900

    #19 command.sh mysql 관련 명령어 추가

--- command.sh
+++ command.sh
@@ -2,6 +2,7 @@
 
 efk_namespace="elastic-system"
 istio_namespace="istio-system"
+db_namespace="db-system"
 values_file="values.yaml"
 
 kubectl create -f https://download.elastic.co/downloads/eck/2.8.0/crds.yaml
@@ -15,6 +16,8 @@ helm install efk .\charts\EFK -n elastic-system
 #helm install elasticsearch-curator ./charts/EFK/ElasticsearchCurator -n $efk_namespace -f $values_file
 
 helm install istio .\charts\istio -n istio-system
+
+helm install mysql .\charts\mysql -n db-system
 
 #helm uninstall efk -n elastic-system --purge
 #bin/elasticsearch-users useradd username -p password -r superuser
